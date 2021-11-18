721c3908389ccc690f4965833c957dd5c900d82a
LeoDias-0 <leodiasleodias00@gmail.com>
2021-11-18 16:51:12 -0300

    Solving ESLint indentation error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,16 @@ import ResetCSS from './utils/ResetCSS'
 import Home from './pages/Home'
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <ResetCSS />
-            <Switch>
-                <Route from='/' exact>
-                    <Home />
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    )
+	return (
+		<BrowserRouter>
+			<ResetCSS />
+			<Switch>
+				<Route from='/' exact>
+					<Home />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	)
 }
 
 export default App
