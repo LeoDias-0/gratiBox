1bacfffbbbf91623020c03724f88d3552b5c0a3e
Bruna Topan <brutopanb@gmail.com>
2021-11-26 12:35:36 -0300

    Preparando review

--- src/validations/validateSignInInfo.js
+++
@@ -1,9 +0,0 @@
-import joi from 'joi'
-
-
-const validateSignInInfo = joi.object({
-	email: joi.string().email({ tlds: { allow: false } }).required(),
-	password: joi.string().min(6).max(60).required()
-}).length(2)
-
-export default validateSignInInfo
